beda2533f80f2b39000a88b2c23949dd680ba537
Charlie Gillions <charliegi@mo.co.uk>
2026-07-28 12:36:08 +0100

    Revert "scheduled for 8am daily"
    This reverts commit ef574b2aa095fb3a99c8db16bf2b9a6e0d45a9d9.

--- agent/schedules/dispatch-reviews.ts
+++ agent/schedules/dispatch-reviews.ts
@@ -3,7 +3,7 @@ import { defineSchedule } from "eve/schedules";
 import { runScheduledReviewDispatch } from "../lib/eve/scheduled-review-dispatch.ts";
 
 export default defineSchedule({
-  cron: "0 8 * * 1-5",
+  cron: "0 7 * * 1-5",
   run({ receive, waitUntil, appAuth }) {
     waitUntil(runScheduledReviewDispatch({ receive, appAuth }));
   },
